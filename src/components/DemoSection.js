274c091c771c3fd743fd974bc0a96cab4356d0f9
Mudit_Khanna <Muditkhannatech@gmail.com>
2024-04-29 19:19:58 +0530

    resolved some errors

--- src/components/DemoSection.js
+++ src/components/DemoSection.js
@@ -1,4 +1,5 @@
-export default function SparkleIcon(){
+import SparkleIcon from "./SparkleIcon"
+export default function DemoSection(){
     return (
         <section className="flex justify-around mt-8 sm:mt-12 items-center">
         <div className=" hidden sm:block bg-gray-800/50 w-[240px] h-[480px] rounded-xl"></div>
